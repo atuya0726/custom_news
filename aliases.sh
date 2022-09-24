39e3c612a384ead9c5afa3ec94ea04923f677f96
atuya <atuya07261008@gmail.com>
2022-09-24 20:07:27 +0900

    [add] new alias

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,6 @@
 alias cn-dev="docker-compose build app && docker-compose up app"
-alias cn-bash="docker exec -it custom-news bash"
+alias cn-bash="docker exec -it custom_news bash"
 alias cn-bash-unable="docker-compose run --rm app bash"
 
 alias cn-front-dev="docker-compose build front && docker-compose up front"
+alias cn-front-bash="docker exec -it custom_news_front bash"
