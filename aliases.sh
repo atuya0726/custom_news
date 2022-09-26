0e4f7d528e2651e9192bd75f0c356946ccc4ca99
atuya <atuya07261008@gmail.com>
2022-09-26 20:42:09 +0900

    [tidy] minor change

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
-alias cn-dev="docker-compose build app && docker-compose up app"
-alias cn-bash="docker exec -it custom_news bash"
-alias cn-bash-unable="docker-compose run --rm app bash"
+alias cn-back-dev="docker-compose build back && docker-compose up back"
+alias cn-back-bash="docker exec -it custom_news bash"
+alias cn-bash-unable="docker-compose run --rm back bash"
 
 alias cn-front-dev="docker-compose build front && docker-compose up front"
 alias cn-front-bash="docker exec -it custom_news_front bash"
